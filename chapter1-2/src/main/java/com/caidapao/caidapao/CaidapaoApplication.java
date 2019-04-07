27b7ba9cb4545f814efd6b19239bf5a8eed0b4b0
caidapao <1014888954@qq.com>
2019-04-07 22:57:02 +0800

    配置文件详解：自定义属性、随机数、多环境配置等

---
+++ chapter1-2/src/main/java/com/caidapao/caidapao/CaidapaoApplication.java
@@ -0,0 +1,13 @@
+package com.caidapao.caidapao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaidapaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaidapaoApplication.class, args);
+    }
+
+}
---
+++ chapter1-2/src/test/java/com/caidapao/caidapao/CaidapaoApplicationTests.java
@@ -0,0 +1,28 @@
+package com.caidapao.caidapao;
+
+import com.caidapao.caidapao.web.TestProperties;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CaidapaoApplicationTests {
+
+    @Autowired
+    private TestProperties properties;
+
+    @Test
+    public void contextLoads() {
+        System.out.println("启动的端口号："+properties.getServerPort());
+        System.out.println(properties.getDescr());
+        Assert.assertEquals(properties.getName(),"蔡大炮");
+        Assert.assertEquals(properties.getAddress(),"123");
+
+
+    }
+
+}
