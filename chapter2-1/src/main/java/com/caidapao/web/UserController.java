551795f381ee8e87d784f6b84c8d9c1b0f41cc2b
“caidapao <1014888954@qq.com>
2019-04-17 17:12:00 +0800

    1.Spring Boot构建RESTful API(GET/POST/PUT/DELETE等请求方式) 2.@RestController/@RequestMapping/@PathVariable/@ModelAttribute/@RequestBody等注解的示例 3.单元测试中，如何发送参数为Json格式的请求

--- chapter2-1/src/main/java/com/caidapao/web/UserController.java
+++ chapter2-1/src/main/java/com/caidapao/web/UserController.java
@@ -1,22 +1,86 @@
 package com.caidapao.web;
 
 import com.caidapao.entity.User;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by caixuan on 2019/4/9.
  * Time 23:38
  */
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
     //创建线程安全的Map
-    private static Map<Long, User> users = Collections.synchronizedMap(new HashMap<Long,User>());
+    private static Map<Long, User> users = Collections.synchronizedMap(new HashMap<>());
+
+    /**
+     *  GET请求
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public List<User> getUserList() {
+        return new ArrayList<>(users.values());
+    }
+
+    /**
+     * POST请求
+     * ModelAttribute注解自动将参数中的值映射到User对应的属性下，
+     *    如 ：localhost:8080/users?name=123&sex=男 将会映射到user对象的 name和sex字段中
+     *    此处的 ModelAttribute注解也可以省略。
+     */
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String postUserView(@ModelAttribute User user) {
+        users.put(user.getId(), user);
+        return "user_detail";
+    }
+
+    /**
+     *  GET请求
+     *  通过PathVariable 注解绑定URL参数
+     *  如：localhost:8080/users/12  12将会绑定到id这个形参上
+     * @param id 用户id
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public User getUser(@PathVariable Long id) {
+        return users.get(id);
+    }
+
+    /**
+     * PUT 请求，意为修改、或者覆盖
+     * @param id 用户Id
+     * @param user  用户
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public String putUser(@PathVariable Long id, @ModelAttribute User user) {
+        User u = users.get(id);
+        u.setName(user.getName());
+        u.setSex(user.getSex());
+        users.put(id, u);
+        return "user_detail";
+    }
+
+    /**
+     * DELETE请求
+     * @param id 用户id
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public String deleteUser(@PathVariable Long id) {
+        users.remove(id);
+        return "user_detail";
+    }
+
+    /**
+     * POST请求
+     *  使用RequestBody注解来接受参数，大都数用来接收JSON格式的参数
+     * @param user 用户
+     */
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public List<User> saveUser(@RequestBody User user) {
+        users.put(user.getId(), user);
+        return new ArrayList<>(users.values());
+    }
+
 
 }
--- chapter2-1/src/test/java/com/caidapao/CaidapaoApplicationTests.java
+++ chapter2-1/src/test/java/com/caidapao/CaidapaoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.caidapao;
 
+import com.alibaba.fastjson.JSONObject;
+import com.caidapao.entity.User;
 import com.caidapao.web.UserController;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,10 +10,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,7 +30,65 @@ public class CaidapaoApplicationTests {
     }
 
     @Test
-    public void getHello() throws Exception {
+    public void testUserController() throws Exception {
+        RequestBuilder requestBuilder = null;
+
+        //1.get方式查看现在的用户列表，应该是一个空数组
+        requestBuilder = get("/users/");
+        mockMvc.perform(requestBuilder).andExpect(status().isOk()).andExpect(content().string(equalTo("[]")));
+
+        //2.post方式保存一条
+        requestBuilder = post("/users/")
+                .param("id", "1")
+                .param("name", "测试大师")
+                .param("sex", "男");
+        mockMvc.perform(requestBuilder).andExpect(content().string(equalTo("user_detail")));
+
+        // 3、get获取user列表，应该能看到刚刚的记录
+        requestBuilder = get("/users/");
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"测试大师\",\"sex\":\"男\"}]")));
+
+        // 4、put修改id为1的user
+        requestBuilder = put("/users/1")
+                .param("name", "测试终极大师")
+                .param("sex", "女");
+        mockMvc.perform(requestBuilder)
+                .andExpect(content().string(equalTo("user_detail")));
+
+        // 5、get一个id为1的user
+        requestBuilder = get("/users/1");
+        mockMvc.perform(requestBuilder)
+                .andExpect(content().string(equalTo("{\"id\":1,\"name\":\"测试终极大师\",\"sex\":\"女\"}")));
+
+        // 6、del删除id为1的user
+        requestBuilder = delete("/users/1");
+        mockMvc.perform(requestBuilder)
+                .andExpect(content().string(equalTo("user_detail")));
+
+        // 7、get查一下user列表，应该为空
+        requestBuilder = get("/users/");
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[]")));
+
+        // 8、post JSON格式参数的请求，保存用户。这里使用了阿里巴巴的JSONUtil将bean转成JsonString，同时contentType要设置成ApplicantJson
+        // 然后查一下user列表，应该有刚刚的caidapao
+
+        User user = new User();
+        user.setSex("男");
+        user.setName("caidapao");
+        user.setId(123L);
+        requestBuilder = post("/users/save/").contentType(MediaType.APPLICATION_JSON).content(JSONObject.toJSONString(user));
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk());
+
+        requestBuilder = get("/users/");
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[{\"id\":123,\"name\":\"caidapao\",\"sex\":\"男\"}]")));
+
 
     }
 
